5ad4e77aff25434e4dbe57d20b57d2bfc1d16d0a
Anton Zlygostev <zlygostev@hotmail.com>
2025-11-10 10:46:59 +0700

    fixing the unlucky lab07 test

--- lab07/tests/index.test.ts
+++ lab07/tests/index.test.ts
@@ -50,6 +50,6 @@ describe('Testing simplify function', ()=>
     test("--42=>42", 4, parseSimplifyAndCost, 0, "--42", commutativeIdentities)
     test("x*0*y=>0", 4, parseSimplifyAndCost, 0, "x*0*y", commutativeIdentities);
     test("x*(1+0*y)=>x", 4, parseSimplifyAndCost, 1, "x*(1+0*y)", commutativeIdentities);
-    test("(a+b)*(b+a)-(a+b)*(a-b)=>2*a*b", 5, parseSimplifyAndCost, 4, "(a+b)*(b+a)-(b+a)*(a-b)", toughIdentities);
+    test("(a+b)*(b+a)-(a-b)*(a-b)=>4*a*b", 5, parseSimplifyAndCost, 4, "(a+b)*(b+a)-(a-b)*(a-b)", toughIdentities);
 });
 
